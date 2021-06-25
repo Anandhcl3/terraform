530c76733d4e11a8e87bae8054c533336b3d48eb
Anandhcl3 <anandhcl3@gmail.com>
2021-06-25 18:29:41 +0530

    fixed terraform errors

--- pre-requisites/main.tf
+++ pre-requisites/main.tf
@@ -1,5 +1,5 @@
 module "aws_pre-requisites" {
-  source = "../modules/pre-requisites"
+  source = "../modules/pre-requsities"
   bucket_name = var.bucket_name
   dynamodb_table_name = var.dynamodb_table_name
 }
--- pre-requisites/terraform.tfvars
+++ pre-requisites/terraform.tfvars
@@ -1,2 +1,2 @@
-bucket_name = devops_terraform_state_bucket
-dynamodb_table_name = 
+bucket_name = "devops_terraform_state_bucket"
+dynamodb_table_name = "terraform-up-and-running-locks"
