af7d6d4ea214061a48ca1b76452fec7d9477cf20
Anandhcl3 <anandhcl3@gmail.com>
2021-07-06 21:02:07 +0530

    updated variables syntax

--- wordpress-standalone/main.tf
+++ wordpress-standalone/main.tf
@@ -1,8 +1,8 @@
 module "wordpress-standalone" {
   source = "../modules/wordpress-standalone"
-  server_port = "var.server_port"
-  environment = "var.environment"
-  project    = "var.project"
-  ami_id     = "var.ami_id"
-  web_instance_type = "var.web_instance_type"
+  server_port = var.server_port
+  environment = var.environment
+  project    = var.project
+  ami_id     = var.ami_id
+  web_instance_type = var.web_instance_type
 }
