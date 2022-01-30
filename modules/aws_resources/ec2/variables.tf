acdc2304ee67b6cb6298ad16b4190388e528c87a
Anandhcl3 <anandhcl3@gmail.com>
2022-01-30 15:56:54 +0530

    files has been updated

---
+++ modules/aws_resources/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "region_aws" {
+    default = "ap-south-1"
+}
+
+variable "ami_id" {
+    default =  "ami-03fa4afc89e4a8a09"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
---
+++ modules/aws_resources/vpc/variables.tf
@@ -0,0 +1,3 @@
+variable "region_aws" {    
+    default = "ap-south-1"
+}
